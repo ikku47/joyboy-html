e4dbbd520c0e92c7ae99bf7c7f7ed95f4a34f3f3
Ameer Rizvi <63542458+AmeerRizvi@users.noreply.github.com>
2023-12-25 00:02:43 +0400

    react countup

--- src/components/FunFact/index.jsx
+++ src/components/FunFact/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Div from '../Div'
-import './funfact.scss'
-import CountUp from 'react-countup';
-
+import React from "react";
+import Div from "../Div";
+import "./funfact.scss";
+import CountUp from "react-countup";
 
 export default function FunFact({ variant, title, subtitle, data }) {
   return (
-    <Div className={variant ? `cs-funfact_wrap ${variant}` : 'cs-funfact_wrap'}>
-      <Div className="cs-funfact_shape" style={{ backgroundImage: 'url(./images/funfact_shape_bg.svg)' }} />
+    <Div className={variant ? `cs-funfact_wrap ${variant}` : "cs-funfact_wrap"}>
+      <Div
+        className="cs-funfact_shape"
+        style={{ backgroundImage: "url(./images/funfact_shape_bg.svg)" }}
+      />
       <Div className="cs-funfact_left">
         <Div className="cs-funfact_heading">
           <h2>{title}</h2>
@@ -18,16 +20,16 @@ export default function FunFact({ variant, title, subtitle, data }) {
         <Div className="cs-funfacts">
           {data.map((item, index) => (
             <Div className="cs-funfact cs-style1" key={index}>
-            <Div className="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color"><span/><CountUp
-                start={1}
-                end={item.factNumber}
-              >
-                {({ countUpRef, start }) => (
-                  <div>
-                    <span ref={countUpRef} />
-                  </div>
-                )}
-              </CountUp></Div>
+              <Div className="cs-funfact_number cs-primary_font cs-semi_bold cs-primary_color">
+                <span />
+                <CountUp start={1} end={item.factNumber}>
+                  {({ countUpRef }) => (
+                    <div>
+                      <span ref={countUpRef} />
+                    </div>
+                  )}
+                </CountUp>
+              </Div>
               <Div className="cs-funfact_text">
                 <span className="cs-accent_color">+</span>
                 <p>{item.title}</p>
@@ -37,5 +39,5 @@ export default function FunFact({ variant, title, subtitle, data }) {
         </Div>
       </Div>
     </Div>
-  )
+  );
 }
