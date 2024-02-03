d18139ece45085cc75d59fb44bdca95fa6b7d699
IKKU <iqbalsuperfuture@gmail.com>
2024-02-03 13:53:41 +0400

    snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css"; 
 import './scss/index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
+
+
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<React.StrictMode>
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+ </React.StrictMode>, rootElement);
+} else {
+  render(<React.StrictMode>
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+ </React.StrictMode>, rootElement);
+}
